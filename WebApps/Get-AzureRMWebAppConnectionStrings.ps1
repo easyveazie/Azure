fdc8da9bbfacf8a053b378aa79e82cc0d999824e
Michael Veazie <michael.veazie@genmills.com>
2017-03-02 11:02:50 -0600

    Add web app slots to report

--- WebApps/Get-AzureRMWebAppConnectionStrings.ps1
+++ WebApps/Get-AzureRMWebAppConnectionStrings.ps1
@@ -9,10 +9,40 @@
 
 Import-Module AzureRM.Websites
 
-#Log in with your Azure account
-Add-AzureRmAccount
+<#
+    .DESCRIPTION
+        Gets connection string information from an Azure Web App or Web App Slot
+#>
+Function Get-AzureRMWebAppConnectionString
+{
+    Param(
+        $WebApp
+    )
 
-$azuWebApps = Get-AzureRmWebApp
+    $results = @()
+
+    foreach($connStr in $WebApp.SiteConfig.ConnectionStrings)
+    {
+        $results += ([PSCustomObject]@{
+            'ResourceGroup' = $WebApp.ResourceGroup
+            'Site' = $WebApp.SiteName            
+            'ConnStrType' = $connStr.Type
+            'ConnStrName' = $connStr.Name
+            'ConnStrValue' = $connStr.ConnectionString
+            'State' = $WebApp.State
+            'Type' = $WebApp.Type
+            'DefaultHostName' = $WebApp.DefaultHostName
+            'EnabledHostNames' = ($WebApp.EnabledHostNames -join ';')
+        })
+    }
+
+    return $results
+}
+
+#Log in with your Azure account
+#Add-AzureRmAccount
+
+#$azuWebApps = Get-AzureRmWebApp
 
 #We'll just pop these up on notepad at the end
 $connStrResults = @()
@@ -21,19 +51,30 @@ foreach($azuWebApp in $azuWebApps)
 {
     #For some reason the global Get command doesn't retrieve these settings. You need to specify the specific Web App name
     $azuWebApp.SiteName | Out-Host
-    $azuWebApp = Get-AzureRmWebApp -Name $azuWebApp.Name -ResourceGroupName $azuWebApp.ResourceGroup
+    $azuWebApp = Get-AzureRmWebApp -Name $azuWebApp.Name -ResourceGroupName $azuWebApp.ResourceGroup -ErrorAction:Continue
  
-    foreach($connStr in $azuWebApp.SiteConfig.ConnectionStrings)
+    $connStrResults += Get-AzureRMWebAppConnectionString -WebApp $azuWebApp
+    
+    $azuWebAppSlots = Get-AzureRmWebAppSlot -WebApp $azuWebApp -ErrorAction:Continue
+
+    #Let's also get each slotted site settings
+    foreach($azuWebAppSlot in $azuWebAppSlots)
     {
-        $connStrResults += ([PSCustomObject]@{
-            'ResourceGroup' = $azuWebApp.ResourceGroup
-            'Site' = $azuWebApp.SiteName
-            'DefaultHostName' = $azuWebApp.DefaultHostName
-            'ConnStrType' = $connStr.Type
-            'ConnStrName' = $connStr.Name
-            'ConnStrValue' = $connStr.ConnectionString
-        })
-    }    
+        try
+        {
+            #All Web Apps have a default staging slot exposed through this API. It doesn't always exist.
+            $azuWebAppSlot = Get-AzureRmWebAppSlot -Name $azuWebAppSlot.Name -ResourceGroupName $azuWebAppSlot.ResourceGroup -ErrorAction:SilentlyContinue
+            
+            if($?)
+            {
+                $connStrResults += Get-AzureRMWebAppConnectionString -WebApp $azuWebAppSlot
+            }
+        }
+        catch
+        {
+            $_.Exception.Message | Write-Error
+        }
+    }
 }
 
 if($connStrResults.Count -eq 0)
